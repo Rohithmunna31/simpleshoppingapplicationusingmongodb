c78f354a080d8cf0ca96c6e00c2b6197667afbf7
Rohithmunna31 <rohithkasna31@gmail.com>
2024-04-12 16:43:54 +0530

    Added delete cart functionality

--- models/User.js
+++ models/User.js
@@ -55,6 +55,41 @@ class User {
       { $set: { cart: updatedCart } }
     );
   }
+
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.item.map((i) => {
+      return i.productId;
+    });
+    return db
+      .collection("products")
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        return products.map((p) => {
+          return {
+            ...p,
+            quantity: this.cart.items.find((i) => {
+              return i.productId.toString() === p._id;
+            }).quantity,
+          };
+        });
+      });
+  }
+
+  deleteItemFromCart(productId) {
+    const updatedCartitems = this.cart.items.filter((item) => {
+      return item.productId.string() !== productId;
+    });
+
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { id: new ObjectId(this._id) },
+        { $set: { cart: { items: updatedCartitems } } }
+      );
+  }
 }
 
 module.exports = User;
